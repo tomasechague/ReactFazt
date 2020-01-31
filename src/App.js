8ff22e8a65c79932b9d610cc8b7d6bf4a7b1a1f3
tomase <tomase@cooperativalaplata.com.ar>
2020-01-31 15:05:02 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import tasks from './sample/task.json';
+import Tasks from './components/Tasks';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component{
+  state = {
+    tasks
+  }
+  render(){
+    return <div>
+      <Tasks tasks={this.state.tasks}/>
     </div>
-  );
+  }
 }
 
 export default App;
---
+++ src/components/Task.js
@@ -0,0 +1,36 @@
+import React,{Component} from 'react';
+
+class Task extends Component{
+
+    styleCompleted(){
+        return{
+            fontSize:'20px',
+            color: this.props.task.done ? 'gray' : 'black',
+            textDecoration:this.props.task.done ? 'line-through':'none'
+        }
+    }
+
+    render(){
+        const {task} = this.props
+        return <p style={this.styleCompleted()}>
+            {task.title} - 
+            {task.description} - 
+            {task.done} - 
+            {task.id}
+            <input type="checkbox"/>
+            <button style={btnDelete}>X</button>
+        </p>
+    }
+}
+
+const btnDelete={
+    fontSize: '18px',
+    background: '#ea2027',
+    color: '#fff',
+    border:'none',
+    padding:'10px 15px',
+    borderRadius:'50%',
+    cursor:'pointer',
+}
+
+export default Task;
